7abbf35c1c7f467922e977848f274dcf173e4529
meokisama <hoangdinhsang123@gmail.com>
2020-04-16 23:42:08 +0700

    5th fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2017-09-16T18:41:00"),
-music = ['sparkle', 'nandemonaiya'];
+music = ['sparkle'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
